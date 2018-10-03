38034f52d724fb4d1e0f0b15f8ee1135909d42be
Jackson Duke <JacksonBDuke@gmail.com>
2018-10-03 16:43:01 -0500

    Working on adding tests.

--- src/test/java/com/example/shoppingcart/controller/CartControllerTest.java
+++ src/test/java/com/example/shoppingcart/controller/CartControllerTest.java
@@ -1,8 +1,13 @@
 package com.example.shoppingcart.controller;
 
 import com.example.shoppingcart.model.CartEntry;
+import com.example.shoppingcart.model.Product;
+import com.example.shoppingcart.repository.CartRepository;
+import com.example.shoppingcart.repository.ProductRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,8 +17,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -23,10 +32,16 @@ public class CartControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
-//    @Test
-//    public void testListAll() throws Exception {
-//
-//    }
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    CartRepository cartRepository;
+
+    @Test
+    public void testListAll() throws Exception {
+
+    }
 
     @Test
     public void testAddToCart() throws Exception {
--- src/test/java/com/example/shoppingcart/controller/ProductControllerTest.java
+++ src/test/java/com/example/shoppingcart/controller/ProductControllerTest.java
@@ -1,4 +1,49 @@
 package com.example.shoppingcart.controller;
 
+import com.example.shoppingcart.model.Product;
+import com.example.shoppingcart.repository.ProductRepository;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.Mockito.when;
+
 public class ProductControllerTest {
+
+    private static String resourceUrl = "http://localhost:8080/api/v1/cart";
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void testListAll() {
+        List<Product> productlist = new ArrayList<>();
+        Product product = new Product();
+        product.setPID(3);
+        product.setName("TestProduct");
+        product.setDescription("A very tasty test");
+        product.setPrice(3.0d);
+        productlist.add(product);
+
+        when(productRepository.findAll()).thenReturn(productlist);
+        HttpEntity<String> request = new HttpEntity("");
+        ResponseEntity<List> response = testRestTemplate.exchange(resourceUrl, HttpMethod.GET, request, List.class);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody(), is(notNullValue()));
+    }
 }
